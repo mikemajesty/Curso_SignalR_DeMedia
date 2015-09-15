cbed8d3d93b5e4b79bf53a4d0fb89ab446649f88
mikemajesty <mikemajesty2011@gmail.com>
2015-09-15 12:04:30 -0300

    MoveShapeHub

---
+++ SignalR_projeto1/MoveShapeHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR_projeto1
+{
+    public class MoveShapeHub : Hub
+    {
+        public void UpdateModel(ShapeModel clienteModel)
+        {
+
+            clienteModel.LastUpdateBy = Context.ConnectionId;
+            Clients.AllExcept(clienteModel.LastUpdateBy).updateShape(clienteModel);
+        }
+        public class ShapeModel
+        {
+
+            [JsonProperty(propertyName:"left")]
+            public double Left { get; set; }
+            [JsonProperty(propertyName:"top")]
+            public double Top { get; set; }
+            [JsonIgnore]
+            public string LastUpdateBy { get; set; }
+        }
+    }
+}
