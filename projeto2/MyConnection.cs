e535a47b0ff088da821c4e2ff87399ae06778a06
mikemajesty <mikemajesty2011@gmail.com>
2015-09-17 00:06:08 -0300

    Chat

---
+++ projeto2/MyConnection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+namespace projeto2
+{
+    public class MyConnection : PersistentConnection
+    {
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data);
+        }
+    }
+}
