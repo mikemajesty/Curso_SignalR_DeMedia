b538cb78575c5f2497dbffc4cc616d1b86b1be04
mikemajesty <mikemajesty2011@gmail.com>
2015-09-22 19:56:27 -0300

    Estrutura Projeto4

---
+++ projeto4/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(projeto4.Startup))]
+
+namespace projeto4
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+        }
+    }
+}
