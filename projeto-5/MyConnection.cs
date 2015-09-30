4a8cab662ba94a30fce73829b6855da58e6ab3ea
mikemajesty <mikemajesty2011@gmail.com>
2015-09-30 09:51:54 -0300

    Cliente: enviando mensagens - Curso de ASP.NET SignalR - Aula 7

---
+++ projeto-5/MyConnection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projeto_5
+{
+    public class MyConnection : PersistentConnection
+    {
+
+    }
+}
---
+++ projeto-5/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(projeto_5.Startup))]
+
+namespace projeto_5
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR<MyConnection>(path:"connect");
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
