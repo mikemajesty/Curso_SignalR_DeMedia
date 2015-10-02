328f76a62baf441e675d9d0b39c4623ba3b5a9a7
mikemajesty <mikemajesty2011@gmail.com>
2015-10-01 22:42:51 -0300

    Estrutura: Exemplo prático - Curso de ASP.NET SignalR - Aula 9

---
+++ projeto6/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(projeto6.Startup))]
+
+namespace projeto6
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR<MyConnection>("/conect");
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
