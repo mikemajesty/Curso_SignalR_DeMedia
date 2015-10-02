81d0678b88cb3885b35e819a82e6978e6f1bbff3
mikemajesty <mikemajesty2011@gmail.com>
2015-10-01 23:14:00 -0300

    Mapeando movimento do mouse: Exemplo prático - Curso de ASP.NET SignalR - Aula 9. Final

--- projeto6/MyConnection.cs
+++ projeto6/MyConnection.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNet.SignalR;
-
+using System.Threading.Tasks;
 namespace projeto6
 {
     internal class MyConnection : PersistentConnection
     {
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data);
+            //return base.OnReceived(request, connectionId, data);
+        }
+         
     }
 }
---
+++ projeto6/Scripts/track.js
@@ -0,0 +1,38 @@
+﻿$(function () {
+    var connection = $.connection("/conect");
+    connection.start(function () {
+        alert('Conect successful');
+        startTrack();
+    });
+    function startTrack() {
+        $('body').mousemove(function (e) {
+            var data = { x: e.pageX, y: e.pageY, id: connection.id };
+            connection.send(data);
+        });
+    }
+    connection.received(function (data) {
+        data = JSON.parse(data);
+        var domElement = "id: " + data.id;
+        var element = createElementIfNotExists(domElement);
+        $(element).css({ left: data.x, top: data.y }).text(data.id);
+    });
+
+    function createElementIfNotExists(id) {
+        var element = $('#' + id);
+        if (element.length == 0) {
+            element = $("<span class='client' id='" + id + "'></span>");
+            element.css({
+                backgroundcolor:RandomColor(),
+                color:000000
+
+            });
+            $('body').append(element).show();
+        }
+        return element;
+    }
+
+    function RandomColor() {
+        return "rgb(" + Math.round(Math.random() * 255 + 50) + "," + Math.round(Math.random() * 255 + 50) + "," + Math.round(Math.random() * 255 + 50) +")";
+    }
+});
+
