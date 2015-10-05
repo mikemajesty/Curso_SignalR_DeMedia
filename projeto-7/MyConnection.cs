3300031d601e35d7b41e86b576199f2a45af2494
mikemajesty <mikemajesty2011@gmail.com>
2015-10-04 23:23:32 -0300

    Final: Hubs - Servidor: recebendo dados

---
+++ projeto-7/MyConnection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace projeto_7
+{
+    [HubName("myConnection")]
+    public class MyConnection : Hub
+    {
+        public void MySend(string message) => Clients.All.sendMessage(message);
+
+    }
+}
