236cababcec495be64954b2e7579e9b8fab2dafc
mikemajesty <mikemajesty2011@gmail.com>
2015-10-09 11:14:37 -0300

    12  -Hubs - Servidor: enviando dados para todos os clientes

---
+++ project-9/MyConnection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project_9
+{
+    [HubName("myConnection")]
+    public class MyConnection : Hub
+    {        
+
+        public void MySend(MyMessage msg) => Clients.All.sendMessage(msg.Name, msg.Message, msg.Time);
+        public void alertAll(string name,string connectionID)
+        {
+            Clients.AllExcept(connectionID).showAlertAll(name);
+        }
+
+
+        public class MyMessage
+        {
+            public string Name { get; set; }
+            public string Message { get; set; }
+            public string Time { get; set; }
+
+
+        }
+    }
+}
