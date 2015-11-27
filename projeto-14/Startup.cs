5cb2d79fb108b019427034a547d68ed134412484
mikemajesty <mikemajesty2011@gmail.com>
2015-11-27 10:44:35 -0200

    Aula 17 - Aula: Hubs - Cliente: proxies - Curso de SignalR

---
+++ projeto-14/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(projeto_14.Startup))]
+
+namespace projeto_14
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
