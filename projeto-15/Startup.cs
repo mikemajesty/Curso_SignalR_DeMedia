91e93813dfaf85edb88fc8faaaba6532b4aac940
mikemajesty <mikemajesty2011@gmail.com>
2015-11-27 12:38:52 -0200

    Aula 18 - Hubs - Cliente: conectando, desconectando e reconectando

---
+++ projeto-15/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(projeto_15.Startup))]
+
+namespace projeto_15
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR("/myApp",new Microsoft.AspNet.SignalR.HubConfiguration());
+        }
+    }
+}
