894b05135b00a046f87f6cd8294a0a34423e3bbb
mikemajesty <mikemajesty2011@gmail.com>
2015-11-30 22:21:27 -0200

    Aula 20 - Cliente: implementando sem utilizar proxy dinâmico

---
+++ projeto-17/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(projeto_17.Startup))]
+
+namespace projeto_17
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ projeto-17/myServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+namespace projeto_17
+{
+    public class myServer : Hub
+    {
+        public void AlertAll(string menssage)
+        {
+            Clients.All.clientAlertAll(menssage);
+        }
+    }
+}
