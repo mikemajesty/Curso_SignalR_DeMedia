03f7ff6b50801969db5108b675e29954eaf2e4b5
mikemajesty <mikemajesty2011@gmail.com>
2015-12-04 19:15:54 -0200

    Aula 21 -  Hubs – Exemplo prático parte 1

---
+++ projeto-18/Board.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace projeto_18
+{
+    public class Board : Hub
+    {
+        private const int BoardWidth = 250;
+        private const int BoardHeight = 250;
+        private static int[,] _buffer = GetEmptyBuffer();
+        public Task BroadCastPoint(int x, int y)
+        {
+            if (x < 0) x = 0;
+            if (x >= BoardWidth) x = BoardWidth - 1;
+            if (y < 0) y = 0;
+            if (y >= BoardHeight) y = BoardHeight - 1;
+            int color = 0;
+            int.TryParse(Clients.Caller.color, out color);
+            _buffer[x, y] = color;
+            return Clients.Others.DrawPoint(x,y, Clients.Caller.color);
+        }
+        public Task BroadCastClear()
+        {
+            _buffer = GetEmptyBuffer();
+            return Clients.Others.Clear();
+        }
+        private static int[,] GetEmptyBuffer()
+        {
+            var buffer = new int[BoardWidth, BoardHeight];
+            return buffer;
+        }
+        public override Task OnConnected()
+        {
+            return Clients.Caller.Update(_buffer);
+        }
+    }
+}
---
+++ projeto-18/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(projeto_18.Startup))]
+namespace projeto_18
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
