a4dfd9044557d7cfb7c2219c5bd509b4b9f60a03
mikemajesty <mikemajesty2011@gmail.com>
2015-12-06 15:42:28 -0200

    Aula 23 - Conexões Persistentes em threads diferentes I

---
+++ projeto-19/ConnectionSpy.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projeto_19
+{
+    public class ConnectionSpy : PersistentConnection
+    {
+
+    }
+}
---
+++ projeto-19/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(projeto_19.Startup))]
+namespace projeto_19
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
