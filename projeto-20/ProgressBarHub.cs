a82c07b98c11a7dce28c3c6b2cbcda09bf9b46b8
mikemajesty <mikemajesty2011@gmail.com>
2015-12-07 09:26:10 -0200

    Aula 25 - Hubs em threads diferentes I

---
+++ projeto-20/ProgressBarHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projeto_20
+{
+    public class ProgressBarHub : Hub
+    {
+
+    }
+}
--- projeto2/Startup.cs
+++ projeto2/Startup.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(projeto2.Startup))]
-
 namespace projeto2
 {
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            app.MapSignalR<MyConnection>("/echo");
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
         }
     }
 }
