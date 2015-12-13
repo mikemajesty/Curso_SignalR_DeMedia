3fcf0b8f44800d45a144df6ec173a9bc0b4323b9
mikemajesty <mikemajesty2011@gmail.com>
2015-12-13 16:40:15 -0200

    A

---
+++ projeto-24/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(projeto_24.Startup))]
+namespace projeto_24
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
